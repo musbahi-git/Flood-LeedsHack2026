faf889ebaae751681c6f52bfcb37627eba99f074
drak0ner <wolfemusbahi@gmail.com>
2026-02-08 04:24:40 +0000

    added console log

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.css';
 
+// Debug: Print VITE_API_URL at runtime
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
